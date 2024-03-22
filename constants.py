03c8cf8ce10634500e07c114d198dfb329461bbb
Kevin Chau <kevin.chau57@gmail.com>
2024-03-21 22:32:05 -0700

    Add dictionary to map region to server code

---
+++ constants.py
@@ -0,0 +1,18 @@
+regions = {
+    "North America": "NA1",
+    "EU West": "EUW1",
+    "EU North & East": "EUN1",
+    "Japan": "JP1",
+    "Korea": "KR",
+    "Latin America North": "LA1",
+    "Latin America South": "LA2",
+    "Brazil": "BR1",
+    "Oceania": "OC1",
+    "Philippines": "PH2",
+    "Russia": "RU",
+    "Singapore": "SG2",
+    "Thailand": "TH2",
+    "Turkey": "TR1",
+    "Taiwan": "TW2",
+    "Vietnam": "VN2"
+}
