5da78f9850d075de7c8a3ff75db936e2b7c3a973
Kevin Chau <kevin.chau57@gmail.com>
2024-04-22 16:29:07 -0700

    Refactor to make it easier to add new graphs

--- GraphGeneration.py
+++ GraphGeneration.py
@@ -16,14 +16,19 @@ def load_file(file_path: str) -> dict:
     return player_data
 
 
-def createGraphs(player_data: dict) -> list:
+def create_graphs(player_data, graph_funcs) -> list:
     """Returns a list of graphs to be displayed
     :param player_data: A dictionary from a JSON file containing player information
     :returns: A list of html graphs
     """
     graphs = []
-    match_ids = list(player_data.keys())[1:0]
+    for func in graph_funcs:
+        create_graph = func(player_data)
+        graphs.append(create_graph)
+    return graphs
 
+
+def create_duration_graph(player_data: dict) -> str:
     # Scatter Plot: Past 20 Game Duration
 
     # Extract game times
@@ -33,10 +38,7 @@ def createGraphs(player_data: dict) -> list:
     # Extract game times
     date = []
     durations = []
-    game_modes = []
-    game_modes_count = {}
     for match_id in match_ids:
-        game_modes.append(player_data[match_id]['info']['gameMode'])
         match_info_pd = pd.Series(player_data[match_id])
 
         date.append(match_info_pd['info']['gameCreation'])
@@ -44,11 +46,6 @@ def createGraphs(player_data: dict) -> list:
         hr_min_sec = time.strftime("%H:%M:%S", time.gmtime(
             match_info_pd['info']['gameDuration']))
         durations.append(hr_min_sec)
-    for mode in game_modes:
-        if mode in game_modes_count:
-            game_modes_count[mode] += 1
-        else:
-            game_modes_count[mode] = 1
 
     dd_df = pd.DataFrame(data={'date': date, 'duration': durations})
     # Convert all unix times to dates
@@ -60,12 +57,29 @@ def createGraphs(player_data: dict) -> list:
     graph = px.scatter(data_frame=dd_df, x="date",
                        y="duration", title="Duration of Past 20 Games")
     graph_html = graph.to_html(full_html=False)
-    graphs.append(graph_html)
+
+    return graph_html
+
+
+def graphs_gamemodes_dist(player_data: dict) -> str:
+    game_modes = []
+    game_modes_count = {}
+    match_ids = list(player_data.keys())[1:]
+
+    for match_id in match_ids:
+        game_modes.append(player_data[match_id]['info']['gameMode'])
+
+    for mode in game_modes:
+        if mode in game_modes_count:
+            game_modes_count[mode] += 1
+        else:
+            game_modes_count[mode] = 1
 
     game_mode_df = pd.DataFrame({'Game Mode': game_modes_count.keys(),
                                  'Count': game_modes_count.values()})
     pie_graph = px.pie(data_frame=game_mode_df, values='Count', names="Game Mode",
                        title="Game Mode Distribution")
     graph_html = pie_graph.to_html(full_html=False)
-    graphs.append(graph_html)
-    return graphs
+
+    return graph_html
+
